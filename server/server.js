91ce8cc650d3e80f39e2ebf403d330d5e80a40d3
panchaliSam <panchalisam20030117@gmail.com>
2024-03-13 12:18:54 +0530

    implement db connection

---
+++ server/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to photographic app'})
+  })
+
+// routes
+
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
+
